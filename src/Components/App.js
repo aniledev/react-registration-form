90fed9124a8578267c6bd7ec67601f0eecd8e70b
aniledev <elinamcgill@gmail.com>
2021-01-09 11:46:34 -0500

    FEAT: construct registration form

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import "../Styles/App.css";
+import RegistrationForm from "./RegistrationForm";
 
 export default class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <RegistrationForm />
+      </div>
+    );
   }
 }
