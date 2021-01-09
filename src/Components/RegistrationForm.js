a3c8394f48b4e6d60e2c44e33b6c2324da11f49c
aniledev <elinamcgill@gmail.com>
2021-01-09 13:09:08 -0500

    FEAT: construct validation logic that determines if a value meets the criteria or not

--- src/Components/RegistrationForm.js
+++ src/Components/RegistrationForm.js
@@ -16,7 +16,6 @@ export default class RegistrationForm extends Component {
       },
     };
   }
-
   updateName(name) {
     this.setState({ name: { value: name } });
   }
@@ -29,15 +28,44 @@ export default class RegistrationForm extends Component {
     this.setState({ repeatPassword: { value: repeatPassword } });
   }
 
+  //methods to validate fields
+  validateName() {
+    const name = this.state.name.value.trim();
+    if (name.length === 0) {
+      return "Name is required.";
+    } else if (name.length < 3) {
+      return "Name must be at least 3 characters long";
+    }
+  }
+  validatePassword() {
+    const password = this.state.password.value.trim();
+    if ((password.length = 0)) {
+      return "Password is required.";
+    }
+    if (password.length < 6 || password.length > 72) {
+      return "Password must be between 6 and 72 characters long.";
+    } else if (!password.match(/[0-9]/)) {
+      // use a regular expression to check if the password contains at least one number
+      return "Password must contain at least one number.";
+    }
+  }
+  validateRepeatPassword() {
+    const repeatPassword = this.state.repeatPassword.value.trim();
+    const password = this.state.password.value.trim();
+    if (repeatPassword !== password) {
+      return "Passwords do not match.";
+    }
+  }
+
   //event handler to process submission of the form
   handleSubmit(event) {
     event.preventDefault();
     // with a controlled form you can grab the values directly from the state
     const { name, password, repeatPassword } = this.state;
     console.log("Name: ", name.value);
-    console.log("Password: ", name.password);
+    console.log("Password: ", password.value);
     console.log("Repeat Password: ", repeatPassword.value);
-    // if these values pass the validation then potentiall sibmit these values to the server
+    // if these values pass the validation then potential submit these values to the server
   }
 
   // THIS IS AN UNCONTROLLED FORM BECAUSE THE STATE OF THE FORM IS NOT KEPT AND MAINTAINED IN THE REACT STATE; THE VALUES OF THE FORM ARE STORED IN THE DOM ITSELF
